f77e3539c7f0de9fe20351d10bb386b7c278a3e9
Fejiro Ogunje <ogunjefejiro@gmail.com>
2022-11-30 13:43:08 +0100

    updates

--- src/pages/analytics.jsx
+++ src/pages/analytics.jsx
@@ -2,30 +2,7 @@ import { DotsHorizontalIcon, PlusIcon, ViewGridIcon, ViewListIcon } from "@heroi
 import { DashboardLayout, ProjectCard } from "../components";
 
 const Analytics = () => {
-  return (
-    <DashboardLayout>
-      <div className="flex justify-between items-center">
-        <div className="flex gap-1 items-center">
-          <button className="bg-white rounded-lg">
-            <ViewListIcon className="text-[#bbbbbb] w-8" />
-          </button>
-          <button className="bg-[#dbf6fe] rounded-lg">
-            <ViewGridIcon className="text-[#33cff8] w-8" />
-          </button>
-        </div>
-        <button className="btn btn-primary">
-          <PlusIcon className="w-5" />
-        </button>
-      </div>
-      <div className="flex justify-between items-center my-6">
-        <h2 className="text-lg font-semibold">Working(2)</h2>
-        <DotsHorizontalIcon className="w-6" />
-      </div>
-      <div>
-        <ProjectCard />
-      </div>
-    </DashboardLayout>
-  );
+  return <DashboardLayout>Analytics</DashboardLayout>;
 };
 
 export default Analytics;
